8e1ab38c86864d1f16ba3961412c72d29ad4b856
madomasuri <madomasuri@penguin>
2020-04-19 20:42:23 +0800

    restructure and improve map

--- streamlit-dashboard.py
+++ streamlit-dashboard.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
+import pydeck as pdk
 
 #getting latest data
 @st.cache
@@ -11,45 +12,19 @@ def get_data(url):
 
 df_confirmed = get_data('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 df_deaths = get_data('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
-df_recovered = get_data('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+# df_recovered = get_data('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
 country_df = get_data('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv')
 
 #data manipulation
 country_df = country_df.sort_values("Confirmed", ascending = False).reset_index(drop=True)
 
 #title and introduction
-st.markdown('<style>h1{color: red;}</style>', unsafe_allow_html=True)
+# st.markdown('<style>h1{color: red;}</style>', unsafe_allow_html=True)
 st.title("Covid 19 Dashboard")
 st.markdown("Built using python streamlit wtih live data from Johns Hopkins CSSE ")
+st.markdown("Last data update :" + str(country_df['Last_Update'].min()))
 st.header("Quote : PM Muhyiddin tells Malaysians")
 st.markdown("> Just stay at home. No need to go out anywhere. With this, everyone can avoid and help stop the spread of the Covid-19 outbreak and Insyallah, it will be stopped. - 18 Mar 2020")
-
-#Global Counter
-confirmed_total = country_df['Confirmed'].sum()
-death_total = country_df['Deaths'].sum()
-recovered_total = country_df['Recovered'].sum()
-active_total = country_df['Active'].sum()
-# st.markdown('<h1 style="color:blue;">This is a heading</h1>' + str(confirmed_total), unsafe_allow_html=True)
-
-st.header("Global")
-st.markdown("Confirmed: "  + str(confirmed_total))
-st.markdown("Deaths: " + str(death_total))
-st.markdown("Recovered: " + str(recovered_total))
-st.markdown("Active: " + str(active_total))
-
-#Malaysia Counter
-malaysia_value = country_df[country_df['Country_Region'] == 'Malaysia']
-confirmed_malaysia = malaysia_value['Confirmed'].sum()
-death_malaysia = malaysia_value['Deaths'].sum()
-recovered_malaysia = malaysia_value['Recovered'].sum()
-active_malaysia = malaysia_value['Active'].sum()
-
-st.header("Malaysia")
-st.markdown("Confirmed: "  + str(confirmed_malaysia))
-st.markdown("Deaths: " + str(death_malaysia))
-st.markdown("Recovered: " + str(recovered_malaysia))
-st.markdown("Active: " + str(active_malaysia))
-
 
 #Top 10 - Most impacted Country Scatter Graph with Slot
 n = 10
@@ -64,6 +39,26 @@ fig.update_layout(
     width = 700
     )
 st.plotly_chart(fig)
+
+#Global Counter
+confirmed_total = country_df['Confirmed'].sum()
+death_total = country_df['Deaths'].sum()
+recovered_total = country_df['Recovered'].sum()
+active_total = country_df['Active'].sum()
+# st.markdown('<h1 style="color:blue;">This is a heading</h1>' + str(confirmed_total), unsafe_allow_html=True)
+
+#Malaysia Counter
+malaysia_value = country_df[country_df['Country_Region'] == 'Malaysia']
+confirmed_malaysia = malaysia_value['Confirmed'].sum()
+death_malaysia = malaysia_value['Deaths'].sum()
+recovered_malaysia = malaysia_value['Recovered'].sum()
+active_malaysia = malaysia_value['Active'].sum()
+
+st.header("Malaysia Compared to Global")
+st.markdown("Confirmed: "  + str(confirmed_malaysia) + "  [Global: "  + str(confirmed_total) + "]")
+st.markdown("Deaths: " + str(death_malaysia) + "  [Global: " + str(death_total) + "]")
+st.markdown("Recovered: " + str(recovered_malaysia) + "  [Global: " + str(recovered_total) + "]")
+st.markdown("Active: " + str(active_malaysia) + "  [Global: " + str(active_total) + "]")
 
 #Data at a glance with slider
 st.header("Info : Data at a glance")
@@ -172,13 +167,42 @@ plot_cases_of_a_country(country_sel_box)
 
 df_map = country_df[['Country_Region', 'Lat', 'Long_', 'Confirmed']]
 df_map.rename({'Lat': 'lat', 'Long_': 'lon'}, axis='columns', inplace=True)
-df_map = df_map.fillna(0)
+df_map = df_map.dropna()
   
 # Visualization Section
 st.subheader("Map")
-st.map(df_map)
+# st.map(df_map)
 
 
+#map using pydeck
+# Adding code so we can have map default to the center of the data
+# midpoint = (np.average(df_map['lat']), np.average(df_map['lon']))
+
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    df_map,
+    # pickable=True,
+    # opacity=0.8,
+    # stroked=True,
+    # filled=True,
+    # radius_scale=6,
+    # radius_min_pixels=1,
+    # radius_max_pixels=100,
+    # line_width_min_pixels=1,
+    get_position=['lon', 'lat'],
+    get_radius="Confirmed",
+    get_fill_color=[255, 140, 0],
+    # get_line_color=[0, 0, 0],
+)
+
+# Set the viewport location
+view_state = pdk.ViewState(7.3291, 49.6178, zoom=5, bearing=0, pitch=0)
+
+# Render
+# r = pdk.Deck(map_style='mapbox://styles/mapbox/light-v9', layers=[layer], initial_view_state=view_state, tooltip={"text": "{Country_Region}\n Confirmed : {Confirmed}"})
+r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{Country_Region}\n Confirmed Cases: {Confirmed}"})
+r.to_html("scatterplot_layer.html", notebook_display=False)
+st.pydeck_chart(r)
 
 st.markdown('<small>© Copyright 2019, Masri Mustaman</small>', unsafe_allow_html=True)
 st.markdown('<small>https://github.com/masrimustaman</small>', unsafe_allow_html=True)
